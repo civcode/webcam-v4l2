1b7a5cd064976a49096701978901de1a6b311aeb
chris <ivi3001@yahoo.de>
2020-10-04 21:05:15 +0200

    played with processing time measurements

--- grab.cpp
+++ grab.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+//#include <iomanip>
 #include <iostream>
 #include <fstream>
 
@@ -11,6 +13,45 @@
 //#define FPS 60
 
 using namespace std;
+
+#define CLIP(color) (unsigned char)(((color) > 0xFF) ? 0xff : (((color) < 0) ? 0 : (color)))
+
+void v4lconvert_yuyv_to_rgb24(const unsigned char *src, 
+                                     unsigned char *dest,
+                                     int width, int height, 
+                                     int stride)
+{
+    int j;
+
+    while (--height >= 0) {
+        for (j = 0; j + 1 < width; j += 2) {
+            int u = src[1];
+            int v = src[3];
+            int u1 = (((u - 128) << 7) +  (u - 128)) >> 6;
+            int rg = (((u - 128) << 1) +  (u - 128) +
+                    ((v - 128) << 2) + ((v - 128) << 1)) >> 3;
+            int v1 = (((v - 128) << 1) +  (v - 128)) >> 1;
+
+            *dest++ = CLIP(src[0] + u1);
+            *dest++ = CLIP(src[0] - rg);
+            *dest++ = CLIP(src[0] + v1);
+
+            *dest++ = CLIP(src[2] + u1);
+            *dest++ = CLIP(src[2] - rg);
+            *dest++ = CLIP(src[2] + v1);
+
+            //*dest++ = CLIP(src[0] + v1);
+            //*dest++ = CLIP(src[0] - rg);
+            //*dest++ = CLIP(src[0] + u1);
+
+            //*dest++ = CLIP(src[2] + v1);
+            //*dest++ = CLIP(src[2] - rg);
+            //*dest++ = CLIP(src[2] + u1);
+            src += 4;
+        }
+        src += stride - (width * 2);
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -49,19 +90,68 @@ int main(int argc, char** argv)
     cv::cuda::resize(src, dst, cv::Size(XRES*2, YRES*2));
     dst.download(out);
     */
-    
+    unsigned char rgb[640*480*3];
+    while (true) { 
+      buffer = webcam.get_buffer();
+      buffer_ptr = buffer.data;
+      auto start = chrono::high_resolution_clock::now();
+      // conversion takes 9 ms
+      v4lconvert_yuyv_to_rgb24((unsigned char *) buffer.data,
+                             rgb,
+                             640,
+                             480,
+                             3);
+      
+      auto stop = chrono::high_resolution_clock::now();
+      auto duration = chrono::duration_cast<chrono::microseconds>(stop-start);
+      cout << "dt = " << std::fixed << std::setprecision(3) << duration.count()/1000.0f << endl;
+      pt = frame.data;
+      //cv::cvtColor(img, out, cv::COLOR_RGB2BGR);
+      //out = img;
+      cv::Mat tmp;
+      yuyv.copyTo(tmp);
+    }
+    while (true) { 
+      auto start = chrono::high_resolution_clock::now();
+      frame = webcam.frame();
+      auto stop = chrono::high_resolution_clock::now();
+      auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+      cout << "dt = " << duration.count() << endl;
+      pt = frame.data;
+      //cv::cvtColor(img, out, cv::COLOR_RGB2BGR);
+      //out = img;
+      cv::Mat tmp;
+      img.copyTo(tmp);
+    }
 
     size_t cnt;
     while (int key = cv::waitKey(1) != 'q') {
 
-      //frame = webcam.frame();
-      //pt = frame.data;
-      //cv::imshow("img", img);
+      frame = webcam.frame();
+      pt = frame.data;
+      //cv::cvtColor(img, out, cv::COLOR_RGB2BGR);
+      //out = img;
+      cv::Mat tmp1, tmp2, tmp3;
+      img.copyTo(tmp1);
+
+      auto start = chrono::high_resolution_clock::now();
+      frame = webcam.frame();
+      auto stop = chrono::high_resolution_clock::now();
+      auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+      cout << "dt = " << duration.count() << endl;
+
+      pt = frame.data;
+      img.copyTo(tmp2);
+
+      tmp3 = tmp2 - tmp1;
+      if (++cnt%10 == 0)   
+        cv::imshow("img", tmp3);
+      continue;
 
       buffer = webcam.get_buffer();
       buffer_ptr = buffer.data;
       
-      if (++cnt%1 == 0) {  
+      if (++cnt%3 == 0) {  
         cv::cvtColor(yuyv, out, cv::COLOR_YUV2BGR_YUYV);
         
         //src.upload(out);
--- webcam.cpp
+++ webcam.cpp
@@ -77,7 +77,6 @@ static void v4lconvert_yuyv_to_rgb24(const unsigned char *src,
                     ((v - 128) << 2) + ((v - 128) << 1)) >> 3;
             int v1 = (((v - 128) << 1) +  (v - 128)) >> 1;
 
-            // output is BGR
             *dest++ = CLIP(src[0] + u1);
             *dest++ = CLIP(src[0] - rg);
             *dest++ = CLIP(src[0] + v1);
@@ -86,7 +85,6 @@ static void v4lconvert_yuyv_to_rgb24(const unsigned char *src,
             *dest++ = CLIP(src[2] - rg);
             *dest++ = CLIP(src[2] + v1);
 
-            // output is RGB
             //*dest++ = CLIP(src[0] + v1);
             //*dest++ = CLIP(src[0] - rg);
             //*dest++ = CLIP(src[0] + u1);
