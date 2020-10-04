9512fb34e69fc3939788ffd70657dda45f5b4d33
chris <ivi3001@yahoo.de>
2020-10-04 18:59:39 +0200

    changed yuyv2RGB to yuyv2BGR

--- webcam.cpp
+++ webcam.cpp
@@ -77,13 +77,23 @@ static void v4lconvert_yuyv_to_rgb24(const unsigned char *src,
                     ((v - 128) << 2) + ((v - 128) << 1)) >> 3;
             int v1 = (((v - 128) << 1) +  (v - 128)) >> 1;
 
-            *dest++ = CLIP(src[0] + v1);
-            *dest++ = CLIP(src[0] - rg);
+            // output is BGR
             *dest++ = CLIP(src[0] + u1);
+            *dest++ = CLIP(src[0] - rg);
+            *dest++ = CLIP(src[0] + v1);
 
-            *dest++ = CLIP(src[2] + v1);
-            *dest++ = CLIP(src[2] - rg);
             *dest++ = CLIP(src[2] + u1);
+            *dest++ = CLIP(src[2] - rg);
+            *dest++ = CLIP(src[2] + v1);
+
+            // output is RGB
+            //*dest++ = CLIP(src[0] + v1);
+            //*dest++ = CLIP(src[0] - rg);
+            //*dest++ = CLIP(src[0] + u1);
+
+            //*dest++ = CLIP(src[2] + v1);
+            //*dest++ = CLIP(src[2] - rg);
+            //*dest++ = CLIP(src[2] + u1);
             src += 4;
         }
         src += stride - (width * 2);
